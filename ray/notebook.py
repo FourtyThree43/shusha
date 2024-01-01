baedb21ba425b4fe14995c27a4030e8e37623e47
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2024-01-01 09:47:20 +0300

    Add fixed width to sidebar

--- ray/notebook.py
+++ ray/notebook.py
@@ -8,21 +8,18 @@ root = tk.Tk()
 root.geometry("640x480+50+50")
 
 notebook = ttk.Notebook()
-downloads = ttk.PanedWindow(notebook)
+downloads = ttk.PanedWindow(notebook, orient=tk.HORIZONTAL)
 add_downloads = ttk.Frame(notebook)
 notebook.add(downloads, text="DOWNLOADS")
 notebook.add(add_downloads, text="ADD DOWNLOADS")
 
-sidebar = ttk.Frame(downloads, width=160, borderwidth=1)
-sidebar.pack_propagate(True)
-sidebar.pack(side=tk.LEFT, fill=tk.Y)
+sidebar = ttk.Frame(downloads, borderwidth=1, width=250)
 
 dt_cols = ("#", "name", "size", "progress", "status", "down_speed")
 d_tree = ttk.Treeview(downloads, columns=dt_cols, show="headings")
 for h in dt_cols:
     d_tree.heading(h, text=h.replace("_", " ").title())
 
-d_tree.pack(side=tk.LEFT)
 
 downloads.add(sidebar)
 downloads.add(d_tree)
